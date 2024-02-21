18915e0491e02cf26c59ae6555ff15c8d5999e5e
Parash Giri <parashgiri@Parashs-MacBook-Pro.local>
2024-02-21 20:11:48 +0000

    changed code

--- src/components/window/main_view.tsx
+++ src/components/window/main_view.tsx
@@ -1,6 +1,7 @@
 import { AuthProvider } from "@/context/AuthContext";
-import React, { useDeferredValue } from "react";
+import React from "react";
 import Navbar from "../Layout/Navbar/Navbar";
+import { About } from "./about";
 import Brand from "./brand";
 
 import Carousel from "./carousel";
@@ -9,37 +10,18 @@ import OfferProducts from "./offerproduct";
 import Product from "./product";
 import ScrollToTopButton from "./scrollup";
 import WhyWeSection from "./whywe";
-import { UseCarousel, UseProduct } from "@/resources/resources";
-import { Spinner } from "../Layout/Atom/atom";
 
 export const Main_view = () => {
-  const product = useDeferredValue(UseProduct());
-  const carousel = useDeferredValue(UseCarousel());
-
-  let windowContent = <></>;
-
-  if (product.isLoading || carousel.isLoading) {
-    // Show a spinner if data is still loading
-    windowContent = (
-      <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center bg-opacity-40 z-[100]">
-      <Spinner size={24} color="text-light-200" />
-    </div>
-    );
-  }
-  if (product.isSuccess && carousel.isSuccess) {
-    windowContent = (
-      <>
-        <Navbar />
-        <Carousel />
-        <Brand />
-        <OfferProducts />
-        <Product />
-        <WhyWeSection />
-        <Footer />
-        <ScrollToTopButton />
-      </>
-    );
-  }
-
-  return windowContent;
+  return (
+    <>
+      <Navbar />
+      <Carousel />
+      <Brand />
+      <OfferProducts />
+      <Product />
+      <WhyWeSection />
+      <Footer />
+      <ScrollToTopButton />
+    </>
+  );
 };
