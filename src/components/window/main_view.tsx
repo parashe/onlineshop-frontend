1a90fe868721938bfb2ba9531fd8cb20c66e32f1
Parash Giri <parashgiri@Parashs-MacBook-Pro.local>
2024-03-04 21:53:09 +0000

    added changes

--- src/components/window/main_view.tsx
+++ src/components/window/main_view.tsx
@@ -1,31 +1,43 @@
 import { AuthProvider } from "@/context/AuthContext";
-import React, { useDeferredValue } from "react";
+import React, { useState, useEffect, useDeferredValue } from "react";
 import Navbar from "../Layout/Navbar/Navbar";
 import Brand from "./brand";
-
-import Carousel from "./carousel";
 import Footer from "./footer";
 import OfferProducts from "./offerproduct";
 import Product from "./product";
 import ScrollToTopButton from "./scrollup";
 import WhyWeSection from "./whywe";
-import { UseCarousel, UseProduct } from "@/resources/resources";
+import { UseProduct } from "@/resources/resources";
 import { Spinner } from "../Layout/Atom/atom";
 import { Home } from "./home";
 
 export const Main_view = () => {
+  const [initialRender, setInitialRender] = useState(true);
   const product = useDeferredValue(UseProduct());
-  const carousel = useDeferredValue(UseCarousel());
+
+  useEffect(() => {
+    setInitialRender(false); // Set initialRender to false after the first rendering
+  }, []);
 
   let windowContent = <></>;
 
-  if (product.isLoading || carousel.isLoading) {
+  if (product.isLoading && initialRender) {
+    windowContent = (
+      <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center  bg-opacity-40 z-[100]">
+        <Spinner
+          size={20}
+          color="text-light-400"
+          message="Please wait for 50 sec in first loading because of low CPU usage"
+        />
+      </div>
+    );
+  } else if (product.isLoading) {
     windowContent = (
       <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center  bg-opacity-40 z-[100]">
         <Spinner size={20} color="text-light-400" />
       </div>
     );
-  } else if (product.error || carousel.error) {
+  } else if (product.error) {
     windowContent = (
       <div className="container">
         <div className="flex w-full justify-center">
